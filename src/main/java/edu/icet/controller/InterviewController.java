55af621b6bf875a53315f01d1dd70b9e42779f64
UdaraDewshan <dewudara@gmail.com>
2026-02-24 23:03:25 +0530

    Implement get all method in controller package

--- src/main/java/edu/icet/controller/InterviewController.java
+++ src/main/java/edu/icet/controller/InterviewController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/interviewer")
@@ -18,6 +20,11 @@ public class InterviewController {
     public ResponseEntity<InterviewerDTO> addInterviewer(@RequestBody InterviewerDTO interviewerDTO){
         InterviewerDTO savedInterviewer = interviewerService.addInterviewer(interviewerDTO);
         return ResponseEntity.ok(savedInterviewer);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<InterviewerDTO>> getAllInterviewers(){
+        return ResponseEntity.ok(interviewerService.getAllInterviewers());
     }
 
 }
